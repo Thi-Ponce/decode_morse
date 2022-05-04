75fb0f930a243f04bd390a630fa546e69b657237
Thi-Ponce <thi.pponce@gmail.com>
2022-05-04 19:37:55 -0300

    Run and fic linter errors

--- decode.rb
+++ decode.rb
@@ -1,47 +1,35 @@
 #!/usr/bin/env ruby
 
-@dictionary = { '.-' => 'A', '-...' => 'B',  '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I',
-		 '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
-		 '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z',
-		'.----' => 1, '..---' => 2, '...--' => 3, '....-' => 4, '.....' => 5, '-....' => 6, '--...' => 7, '---..' => 8, '----.' => 9, '-----' => 0}
+@dictionary = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+                '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+                '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+                '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+                '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+                '--..' => 'Z', '.----' => 1, '..---' => 2, '...--' => 3, '....-' => 4,
+                '.....' => 5, '-....' => 6, '--...' => 7, '---..' => 8, '----.' => 9,
+                '-----' => 0 }
 
 def decode_char(char)
-	@morse_letter = @dictionary[char]
-	@morse_letter
+  @morse_letter = @dictionary[char]
+  @morse_letter
 end
 
 def decode_word(char)
-	split_char = char.split(' ')
-	@str = ''
-	split_char.each do |i| 
-		@str += decode_char(i)
-	end
-	@str
+  split_char = char.split
+  @str = ''
+  split_char.each do |i|
+    @str += decode_char(i)
+  end
+  @str
 end
 
-
 def decode(words)
-    @split_words = words.split('   ')
-	@str = ''
-    @split_words.each do |word|
-        @str += "#{decode_word(word)} "
-    end
-	@str
+  @split_words = words.split('   ')
+  @str = ''
+  @split_words.each do |word|
+    @str += "#{decode_word(word)} "
   end
+  @str
+end
 
-
-# def decode(words)
-#     @split_words = words.split('   ')
-#     puts @split_words
-#     @split_words.each do |word|
-#     	# puts word
-#     	@str = ''
-#         @str = decode_word(word)
-#         print @str
-#     end
-#   end
-
-
-# decode_char('.-')
-# decode_word("-- -.--")
-decode("-- -.--   -. .- -- .")
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
